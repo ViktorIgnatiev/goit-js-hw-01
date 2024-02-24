16e9a990f170f9332c3117ccd15e163284bca069
Viktor Ignatiev <viktor.ignatiev@wp.pl>
2024-02-24 18:46:09 +0100

    Home Work JS vol II

--- js/task-3.js
+++ js/task-3.js
@@ -12,34 +12,3 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getElementWidth(content, padding, border) {
-//     // Перетворюємо рядки у числа, відкидаючи "px"
-//     const contentWidth = parseFloat(content);
-//     const paddingWidth = parseFloat(padding);
-//     const borderWidth = parseFloat(border);
-
-//     // Обчислюємо загальну ширину елемента, враховуючи box-sizing: border-box
-//     const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-    
-//     return totalWidth;
-// }
-
-// // Приклади виклику функції для перевірки її коректності
-// console.log(getElementWidth("50px", "8px", "4px"));   // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px"));  // 200
